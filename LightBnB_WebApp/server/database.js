ab1cdebaa3ae7cf0f9712f871105c5f74f95c16c
Hind <hind.nayhi@gmail.com>
2022-08-24 17:36:45 -0600

    Setup Node Postgres.

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -1,5 +1,17 @@
 const properties = require('./json/properties.json');
 const users = require('./json/users.json');
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'labber',
+  password: 'labber',
+  host: 'localhost',
+  port: 5432,
+  database: 'lightbnb'
+});
+
+pool.query(`SELECT title FROM properties LIMIT 10;`)
+  .then(response => {console.log(response)})
 
 /// Users
 
